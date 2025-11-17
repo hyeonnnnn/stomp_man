19bdde6648814d921e20b006ff5ea6c4b2dacbd5
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-17 13:20:55 +0900

    feat: 땅인지 적인지에 따라 점프력 증감

--- stomp_man/Assets/02. Scripts/Player/PlayerMove.cs
+++ stomp_man/Assets/02. Scripts/Player/PlayerMove.cs
@@ -4,8 +4,12 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _startbounceForce = 3f;
     private float _bounceForce = 3f;
+    private float _bounceForceIncrease = 0.2f;
+    private float _bounceForceDecrease = 0.7f;
 
     [SerializeField] private float _stompForce = 10f;
+    private bool _isStomp = false;
+    private bool _isAttack = false;
 
     private Rigidbody2D _rigidbody;
 
@@ -22,8 +26,9 @@ public class PlayerMove : MonoBehaviour
 
     private void Stomp()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && _isStomp == false)
         {
+            _isStomp = true;
             _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, -_stompForce);
         }
     }
@@ -32,25 +37,57 @@ public class PlayerMove : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground") == true)
         {
-            Bounce(collision, _bounceForce);
+            if(_isAttack == true)
+            {
+                BounceForceIncrease();
+            }
+            else
+            {
+                BounceForceDecrease();
+            }
+            _isAttack = false;
+            _isStomp = false;
+
+            Bounce();
         }
     }
 
-    private void Bounce(Collision2D collision, float _bounceForce)
+    private void Bounce()
     {
         _rigidbody.AddForce(Vector2.up * _bounceForce, ForceMode2D.Impulse);
+    }
+
+    private void BounceForceIncrease()
+    {
+        Debug.Log("점프력 증가");
+        _bounceForce += _bounceForceIncrease;
+    }
+
+    private void BounceForceDecrease()
+    {
+        Debug.Log("점프력 감소");
+        _bounceForce -= _bounceForceDecrease;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy") == true)
         {
-            Attack(collision);
+            if (_isStomp == true)
+            {
+                Attack(collision);
+                BounceForceIncrease();
+            }
+            else
+            {
+                BounceForceDecrease();
+            }
         }
     }
 
     private void Attack(Collider2D collision)
     {
+        _isAttack = true;
         collision.GetComponent<Enemy>().Die();
     }
 }
