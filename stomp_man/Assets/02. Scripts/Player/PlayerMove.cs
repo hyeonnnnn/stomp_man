27d93fb565cb17ac9795570f97431369d081184d
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-11-17 12:55:59 +0900

    feat: 플레이어가 내려 찍기

--- stomp_man/Assets/02. Scripts/Player/PlayerMove.cs
+++ stomp_man/Assets/02. Scripts/Player/PlayerMove.cs
@@ -2,25 +2,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _bounceForce = 1f;
+    [SerializeField] private float _startbounceForce = 3f;
+    private float _bounceForce = 3f;
+
+    [SerializeField] private float _stompForce = 10f;
 
     private Rigidbody2D _rigidbody;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _bounceForce = _startbounceForce;
+    }
 
+    private void Update()
+    {
+        Stomp();
+    }
+
+    private void Stomp()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, -_stompForce);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground") == true)
         {
-            Bounce(collision);
+            Bounce(collision, _bounceForce);
         }
     }
 
-    private void Bounce(Collision2D collision)
+    private void Bounce(Collision2D collision, float _bounceForce)
     {
         _rigidbody.AddForce(Vector2.up * _bounceForce, ForceMode2D.Impulse);
     }
